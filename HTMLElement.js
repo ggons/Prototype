2aaa6fc15109e5b5cefc7bf017221f486d003303
nyg4628 <nyg4628@gmail.com>
2018-08-13 18:28:13 +0900

    add off

--- HTMLElement.js
+++ HTMLElement.js
@@ -1,10 +1,10 @@
-function on(event, selector, fn) {
+function on(name, selector, fn) {
   if (typeof selector === 'function') {
     fn = selector;
     selector = undefined;
   }
 
-  this.addEventListener(event, (e) => {
+  const a = (e) => {
     const { target } = e;
     if (!selector) {
       if (this === target || this.contains(target)) {
@@ -24,9 +24,72 @@ function on(event, selector, fn) {
         result && fn.call(elements[index], e);
       }
     }
+  }
+
+  this.addEventListener(name, a);
+  let _events = this._events = this._events || [];
+  
+  _events.push({
+    name,
+    selector,
+    fn,
+    a
   });
 
   return this;
 }
 
+function off() {
+  let name;
+  let selector;
+  let fn;
+
+  let filteredEvents = [];
+
+  const _events = this._events;
+  if (!_events) {
+    return false;
+  }
+
+  if (arguments.length === 0) {
+    _events.forEach((event, i) => {
+      filteredEvents.push([i, event]);
+    });
+  }
+
+  if (arguments.length === 1) {
+    name = arguments[0];
+
+    _events.forEach((event, i) => {
+      event.name === name && filteredEvents.push([i, event]);
+    });
+  }
+  
+  if (arguments.length === 2) {
+    name = arguments[0];
+    fn = arguments[1];
+
+    _events.forEach((event, i) => {
+      event.name === name && event.fn === fn && filteredEvents.push([i, event]);
+    });
+  }
+
+  if (arguments.length === 3) {
+    name = arguments[0];
+    selector = arguments[1];
+    fn = arguments[2];
+
+    _events.forEach((event, i) => {
+      event.name === name && event.selector === selector && event.fn === fn && filteredEvents.push([i, event]);
+    });
+  }
+
+  for (let i = filteredEvents.length - 1; i >= 0; i--) {
+    let filteredEvent = filteredEvents[i];
+    this.removeEventListener(filteredEvent[1].name, filteredEvent[1].a);
+    _events.splice(filteredEvent[0], 1);
+  }
+}
+
 HTMLElement.prototype.on = on;
+HTMLElement.prototype.off = off;
